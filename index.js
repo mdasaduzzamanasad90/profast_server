cf7edd1954865c70b374b6f3b438fec76b7cc86e
MD. Asaduzzaman Asad <mdasaduzzamanasadfacebook@gmail.com>
2025-10-25 00:58:28 +0600

    create post method and get method with quary email

--- index.js
+++ index.js
@@ -8,8 +8,7 @@ const port = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 
-const uri =
-  `mongodb+srv://${process.env.USER_NAME}:${process.env.USER_PASS}@cluster0.tobpnew.mongodb.net/?appName=Cluster0`;
+const uri = `mongodb+srv://${process.env.USER_NAME}:${process.env.USER_PASS}@cluster0.tobpnew.mongodb.net/?appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -25,8 +24,71 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
+    const dataBaseParcels = client.db("profast").collection("parcels");
 
+    await dataBaseParcels.createIndex({ trackingId: 1 }, { unique: true });
 
+    app.get("/parcels", async (req, res) => {
+      try {
+        const email = req.query.email;
+        const query = { email: email };
+        // সব parcels ডাটাবেজ থেকে নিয়ে আসা
+        const result = await dataBaseParcels
+          .find(query)
+          .sort({ createdDate: -1 })
+          .toArray();
+
+        // সফল রেসপন্স পাঠানো
+        res.status(200).send({
+          success: true,
+          message: "Parcels fetched successfully",
+          data: result,
+        });
+      } catch (error) {
+        console.error("Error fetching parcels:", error);
+
+        // Error রেসপন্স পাঠানো
+        res.status(500).send({
+          success: false,
+          message: "Failed to fetch parcels",
+          error: error.message,
+        });
+      }
+    });
+
+    app.post("/parcels", async (req, res) => {
+      try {
+        const clientData = req.body;
+
+        const parcelData = {
+          ...clientData,
+          trackingId:
+            "TRK-" + Math.random().toString(36).slice(2, 8).toUpperCase(),
+          createdDate: new Date().toISOString(), // ✅ ISO 8601 ফরম্যাট
+          paymentStatus: "unpaid",
+          parcelStatus: "pending",
+        };
+
+        // ডাটাবেজে ইনসার্ট করা
+        const result = await dataBaseParcels.insertOne(parcelData);
+
+        // সফল হলে রেসপন্স পাঠানো
+        res.status(201).send({
+          success: true,
+          message: "Parcel added successfully",
+          data: result,
+        });
+      } catch (error) {
+        console.error("Error inserting parcel:", error);
+
+        // যদি কোনো ভুল হয়, তা ধরার জন্য proper error response
+        res.status(500).send({
+          success: false,
+          message: "Failed to add parcel",
+          error: error.message,
+        });
+      }
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
